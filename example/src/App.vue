6810acc4441c778ae2b59b2bf330060c4b45952d
laihanbin <512412344@qq.com>
2022-10-08 17:59:24 +0800

    feat: create Dom structure

---
+++ example/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <img src="https://img-squad-prod.humandetail.com/cover/20220925QErvmqBy.jpg" alt="" />
+    <img src="https://img-squad-prod.humandetail.com/cover/20220808an1p7Wd9.jpg" alt="" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { PrettyPreview } from '../../packages/index'
+import '../../packages/pretty-preview/assets/styles/index.scss'
+
+onMounted(() => {
+  const pp = new PrettyPreview({
+    root: '.wrapper'
+  })
+})
+</script>
+
+<style scoped>
+</style>
