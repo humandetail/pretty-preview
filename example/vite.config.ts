036cbf4279d91264671c6c482596bd9d8cc7c020
laihanbin <512412344@qq.com>
2022-10-13 14:56:30 +0800

    fix: update example

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname)
+  console.log(env)
+  return {
+    plugins: [vue()],
+    base: env.VITE_APP_PATHNAME,
+  }
 })
