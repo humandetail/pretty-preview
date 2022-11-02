9eca466ddf995bff6aa78488141be845f26529f0
laihanbin <512412344@qq.com>
2022-11-02 18:00:57 +0800

    chore: bug address

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,9 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, __dirname)
-  console.log(env)
+
   return {
     plugins: [vue()],
-    base: env.VITE_APP_PATHNAME,
+    base: env.VITE_APP_PATHNAME
   }
 })
